05d2057bcbd27216b051f015a7f5393fd26bc647
David Colclazier <david@jdcnetworksolutions.com>
2014-12-22 00:23:19 -0700

    Added basic motion sensor functionality

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Threading;
 using Json.NETMF;
 using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
 using NetduinoApplication4.LCD;
 using NetduinoApplication4.LCD.Transfer_Protocols;
 using SecretLabs.NETMF.Hardware.Netduino;
@@ -22,11 +24,16 @@ namespace NetduinoApplication4
                                                             16));           //Column Count
             var i2CBus = I2CBus.GetInstance();
             var tempSensor = new IrTempSensor(0x5a,58);
-
-            lcd.Write("It is done.",200,true);
             
+            var motionSensor = new MotionSensor(Pins.GPIO_PIN_D7, lcd);
+            motionSensor.Activate();
+           
+            lcd.Write("Alarm Activated.",50,true);
+
+            Thread.Sleep(2000);
             while (true)
             {
+                
                 var registerData = i2CBus.ReadRegister(tempSensor.Config, 
                     (byte)IrTempSensor.RamRegisters.ObjectTempOne, 1000);
                 
@@ -34,9 +41,45 @@ namespace NetduinoApplication4
 
                 lcd.SetCursorPosition(0, 1);
                 lcd.Write(temp.ToString().Substring(0,5));
-                Thread.Sleep(100);               
+
+                Thread.Sleep(100);
+                
             }
-        }        
+        }
+        
+    }
+
+    public class MotionSensor
+    {
+        
+        
+        private readonly InterruptPort _pir;
+        private readonly Lcd _display;
+        
+        public MotionSensor(Cpu.Pin inputPin, Lcd display)
+        {
+            _pir = new InterruptPort(inputPin, false, ResistorModes.PullDown, InterruptModes.InterruptEdgeHigh);
+            _display = display;
+        }
+
+        public void Activate()
+        {
+            _pir.OnInterrupt += PollAlarm;
+        }
+
+        private void PollAlarm(uint data1, uint data2, DateTime time)
+        {
+            _display.ClearDisplay();
+            _display.SetCursorPosition(0, 0);
+            _display.Write("Motion Detected!!");
+            Thread.Sleep(2500);
+            _pir.ClearInterrupt();
+            _display.ClearDisplay();
+            _display.SetCursorPosition(0, 0);
+            _display.Write("All is quiet...");
+        }
+
+        
     }
 }
 
